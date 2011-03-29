a6f244b6dbb2ec75c6c01ac2b018bb76a497511b
Andraz Brodnik <brodul@gmx.com>
2011-03-29 21:00:43 +0200

    moved files

--- tarok/deck.py
+++ tarok/deck.py
@@ -19,16 +19,17 @@ class Card:
     def __repr__(self):
         return str(self.name)
 
+
 class Tarok(Card):
 
     rank_list_tarok = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX",
         "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "IXX",
         "XX", "XXI", "XXII", ]
-    
+
     def __init__(self, suit, rank):
         Card.__init__(self, suit, rank)
         self.name = "Tarok " + self.rank_list_tarok[self.rank]
-    
+
 
 class Suits(Card):
 
@@ -38,14 +39,16 @@ class Suits(Card):
     rank_list_num_black = ["7", "8", "9", "10"]
     rank_list_red = rank_list_num_red + rank_list_both
     rank_list_black = rank_list_num_black + rank_list_both
-    
+
     def __init__(self, suit, rank):
         Card.__init__(self, suit, rank)
-        
+
         if self.suit < 2:
-            self.name = self.rank_list_red[self.rank] + " of " + self.suit_list[self.suit]
+            self.name = self.rank_list_red[self.rank] + " of "
+            + self.suit_list[self.suit]
         if self.suit is 2 or self.suit is 3:
-            self.name = self.rank_list_black[self.rank] + " of " + self.suit_list[self.suit]
+            self.name = self.rank_list_black[self.rank] + " of "
+            + self.suit_list[self.suit]
 
 
 class Deck:
@@ -74,14 +77,13 @@ class Deck:
 
     def pop_card(self):
         return self.cards.pop()
-    
+
 
 class Hand(Deck):
-    
 
     def __init__(self, label):
         self.label = label
         self.cards = []
 
-    def add_cards(self,number):
+    def add_cards(self, number):
         pass
---
+++ tarok/tests.py
@@ -0,0 +1,13 @@
+import random
+
+import unittest
+
+import deck
+
+TAROK_NUM=22
+
+class TestCardCreation(unittest.TestCase):
+
+    def test_tarok_creation(self):
+        
+        
